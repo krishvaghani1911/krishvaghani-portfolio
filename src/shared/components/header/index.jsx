b9efeaf8ebee3cc838757add4787b4bf5d0fb547
krish vaghani <vaghanikrish1911@gmail.com>
2024-07-09 12:21:11 +0530

    name

--- src/shared/components/header/index.jsx
+++ src/shared/components/header/index.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className='header'>
         <div className="container">
-            <button id='main'><h1>Name</h1></button>
+            <button id='main'><h1>Krish Vaghani</h1></button>
             <div className="menu">
                 <div className="about"><a href='#about'>About</a></div>
                 <div className="project"><a href='#resume'>Resume</a></div>
